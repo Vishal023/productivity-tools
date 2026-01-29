5ffc1e59ee4adf6079407922bbcaba913b1ced85
vishal023 <v.singh.kld@gmail.com>
2026-01-29 14:25:50 +0530

    feat: configure GitHub Pages deployment
    - Set base URL to /productivity-tools/
    - Add gh-pages for deployment
    - Switch to HashRouter for GitHub Pages compatibility
    - Add deploy script: npm run deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from '@/components/layout'
 import { HomePage } from '@/pages/home'
 import { StoryPointsPage } from '@/pages/story-points'
@@ -35,8 +35,8 @@ function AppContent() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppContent />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
